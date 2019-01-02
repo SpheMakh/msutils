c8a7687b4f4fd305dc1d8800f07299500df9b228
sphemakh <sphemakh@gmail.com>
2019-01-02 16:01:55 +0200

    Fixes #19

--- MSUtils/msutils.py
+++ MSUtils/msutils.py
@@ -15,6 +15,42 @@ import codecs
 
 dm = pyrap.measures.measures()
 
+STOKES_TYPES = { 
+    0  : "Undefined", 
+    1  : "I", 
+    2  : "Q", 
+    3  : "U", 
+    4  : "V", 
+    5  : "RR", 
+    6  : "RL", 
+    7  : "LR", 
+    8  : "LL", 
+    9  : "XX", 
+    10 : "XY", 
+    11 : "YX", 
+    12 : "YY", 
+    13 : "RX", 
+    14 : "RY", 
+    15 : "LX", 
+    16 : "LY", 
+    17 : "XR", 
+    18 : "XL", 
+    19 : "YR", 
+    20 : "YL", 
+    21 : "PP", 
+    22 : "PQ", 
+    23 : "QP", 
+    24 : "QQ", 
+    25 : "RCircular", 
+    26 : "LCircular", 
+    27 : "Linear", 
+    28 : "Ptotal", 
+    29 : "Plinear", 
+    30 : "PFtotal", 
+    31 : "PFlinear", 
+    32 : "Pangle", 
+}
+
 def summary(msname, outfile=None, display=True):
 
     tab = pyrap.tables.table(msname)
@@ -27,7 +63,7 @@ def summary(msname, outfile=None, display=True):
         "SCAN"      :   {},
         "EXPOSURE"  :   {},
         "NROW"      :   tab.nrows(),
-        "NCOR"      :   tab.getcell('DATA', 0).shape[-1],
+        "CORR"      :   {},
     }
 
     tabs = {
@@ -76,7 +112,18 @@ def summary(msname, outfile=None, display=True):
                 info[key][name] = _tab.getcol(name)
         _tab.close()
 
-    # Get maximum baseline
+    # add correlation information
+    tabcorr = pyrap.tables.table(msname+'::POLARIZATION')
+    corr_type = tabcorr.getcol("CORR_TYPE")[0]
+    info["CORR"]["NUM_CORR"] = tabcorr.getcol("NUM_CORR")[0]
+    info["NCOR"] = info["CORR"]["NUM_CORR"]
+    corrs = []
+    for ct in corr_type:
+        corrs.append( STOKES_TYPES[ct] )
+    info["CORR"]["CORR_TYPE"] = corrs
+    tabcorr.close()
+
+    # get maximum baseline
     uv = tab.getcol("UVW")[:,:2]
     mb = numpy.sqrt((uv**2).sum(1)).max()
     info['MAXBL'] = mb
